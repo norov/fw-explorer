309f9c3cd65b70bdb54dc1e128a713f5b8d26f71
Yury Norov <yury.norov@gmail.com>
2020-06-15 13:38:59 -0700

    abm_logic: cleanup and improve performance
    500 periods / 1000 paths:
    Before:
    25.5  sec
    After:
    0.077 sec
    
    calculate_returns() is implemented as 2-level for-loop. Inner loop ocalculates
    parameters over single path, outer loop iterates paths. We can do it faster by
    stacking all paths into vector and removing outer loop because it allows
    the python core to optimize runtime by enabling vector instructions
    support, better memory utilization etc.

--- dash-fw/abm_logic.py
+++ dash-fw/abm_logic.py
@@ -56,18 +56,30 @@ def update_randomness(nr, sim_L):
     return rand
 
 
-def mean_price(price, period, path, rvmean):
+def mean_price(price, period, rvmean):
     if period >= rvmean:
-        return np.mean(price[period - rvmean : period, path])
+        return np.mean(price[period - rvmean : period, :])
 
     # Assuming starting price is 0
-    return np.mean(price[0 : period, path]) * period / rvmean
+    return np.mean(price[0 : period, :]) * period / rvmean
 
 def calculate_returns(given_params, calibrated_params):
     print(given_params)
     rvmean = given_params["rvmean"]
     nr = given_params["num_runs"]
     sim_L = given_params["periods"]
+    mu = given_params["mu"]
+
+    alpha_w = calibrated_params["alpha_w"]
+    alpha_o = calibrated_params["alpha_O"]
+    alpha_p = calibrated_params["alpha_p"]
+    eta = calibrated_params["eta"]
+    beta = given_params["beta"]
+    phi = calibrated_params["phi"]
+    chi = calibrated_params["chi"]
+
+    sigma_f = calibrated_params["sigma_f"]
+    sigma_c = calibrated_params["sigma_c"]
 
     if os.path.exists('rnd.pickle') == False :
         set_randomness(nr, sim_L)
@@ -83,65 +95,48 @@ def calculate_returns(given_params, calibrated_params):
         r = norm.rvs(loc = 0, scale = 1, size=(2, rand_nr, sim_L - rand_sim_L))
         rand = np.append(rand, r, axis = 2)
 
-    Nf = np.zeros([sim_L, 1])  ##progostic state
-    Df = np.zeros([sim_L, 1])  ##diagnostic state
-    Dc = np.zeros([sim_L, 1])  ##diagnostic state
-    Gf = np.zeros([sim_L, 1])  ##diagnostic state
-    Gc = np.zeros([sim_L, 1])  ##diagnostic state
-    Wf = np.zeros([sim_L, 1])  ##diagnostic state
-    Wc = np.zeros([sim_L, 1])  ##diagnostic state
-    A = np.zeros([sim_L, 1])  ##diagnostic state
-
-    # initial values
-    Nf[0:2] = 0.5
-    ##AK: what else should be i nitialized if we were doing forecasting?
+    Df = np.zeros([sim_L, nr])  ##diagnostic state
+    Dc = np.zeros([sim_L, nr])  ##diagnostic state
+    Gf = np.zeros([sim_L, nr])  ##diagnostic state
+    Gc = np.zeros([sim_L, nr])  ##diagnostic state
+    Wf = np.zeros([sim_L, nr])  ##diagnostic state
+    Wc = np.zeros([sim_L, nr])  ##diagnostic state
+    A = np.zeros([sim_L,  nr])  ##diagnostic state
 
     # add for loop to create ndarray of many runs of exog signal
     Nc = np.zeros([sim_L, nr])  #Number of chartists
     Nf = np.zeros([sim_L, nr])  ##progostic state
     P = np.zeros([sim_L + 1, nr])  ##progostic state
     pstar = np.zeros([sim_L, nr])
-    for r in range(nr):  ##AK: why not start at time t=1
-        ## YN: to make all start from a single point?
-        for t in range(2, sim_L):  # generation of single signal over time
-            # portfolio performance
-            Gf[t] = (np.exp(P[t, r]) - np.exp(P[t - 1, r])) * Df[t - 2]
-            Gc[t] = (np.exp(P[t, r]) - np.exp(P[t - 1, r])) * Dc[t - 2]
+    # for rrrr in range(nr):  ##AK: why not start at time t=1
+    #     ## YN: to make all start from a single point?
+    for t in range(2, sim_L):  # generation of single signal over time
+        # portfolio performance
+        Gf[t] = (np.exp(P[t, :]) - np.exp(P[t - 1, :])) * Df[t - 2]
+        Gc[t] = (np.exp(P[t, :]) - np.exp(P[t - 1, :])) * Dc[t - 2]
 
-            # summarize performance over time
-            Wf[t] = (
-                calibrated_params["eta"] * Wf[t - 1]
-                + (1 - calibrated_params["eta"]) * Gf[t]
-            )
-            Wc[t] = (
-                calibrated_params["eta"] * Wc[t - 1]
-                + (1 - calibrated_params["eta"]) * Gc[t]
-            )
+        # summarize performance over time
+        Wf[t] = eta * Wf[t - 1] + (1 - eta) * Gf[t]
+        Wc[t] = eta * Wc[t - 1] + (1 - eta) * Gc[t]
 
-            # type fractions
-            Nf[t, r] = 1 / (1 + np.exp(-given_params["beta"] * A[t - 1]))
-            Nc[t, r] = 1 - Nf[t, r]
+        # type fractions
+        Nf[t, :] = 1 / (1 + np.exp(-beta * A[t - 1, :]))
+        Nc[t, :] = 1 - Nf[t, :]
 
-            # The A[t] dynamic is set up to handle several models
-            A[t] = (
-                calibrated_params["alpha_w"] * (Wf[t] - Wc[t])
-                + calibrated_params["alpha_O"]
-                + calibrated_params["alpha_p"] * (pstar[t-1, r] - P[t, r]) ** 2
-            )
+        # The A[t] dynamic is set up to handle several models
+        A[t] = alpha_w * (Wf[t, :] - Wc[t, :])\
+                + alpha_o + alpha_p * (pstar[t-1, :] - P[t, :]) ** 2
 
-            # demands
-            Df[t] = calibrated_params["phi"] * (pstar[t-1, r] - P[t, r]) + calibrated_params[
-                "sigma_f"
-            ] * rand[0, r, t]
-            Dc[t] = calibrated_params["chi"] * (P[t, r] - P[t - 1, r]) + calibrated_params[
-                "sigma_c"
-            ] * rand[1, r, t]
+        # demands
+        Df[t, :] = phi * (pstar[t-1, :] - P[t, :]) + sigma_f * rand[0, : nr, t]
+        Dc[t, :] = chi * (P[t, :] - P[t - 1, :]) +   sigma_c * rand[1, : nr, t]
 
-            # pricing
-            P[t + 1, r] = P[t, r] + given_params["mu"] * (Nf[t, r] * Df[t] + Nc[t, r] * Dc[t])
+        # pricing
+        P[t + 1, :] = P[t, :]\
+                + mu * (Nf[t, :] * Df[t, :] + Nc[t, :] * Dc[t, :])
 
-            if rvmean is not  None:
-                pstar[t, r] = mean_price(P, t, r, rvmean)
+        if rvmean is not  None:
+            pstar[t, :] = mean_price(P, t, rvmean)
 
     log_r = P[1 : sim_L + 1, :] - P[0:sim_L, :]
     return log_r, Nc
